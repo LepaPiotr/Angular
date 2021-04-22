9db3ce8ef592558cdfbff8d4d5151c511b7dfbe8
Piotr Lepa <piotr-lepa@o2.pl>
2021-04-22 22:06:37 +0200

    .

---
+++ src/app/get-products.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './Entity/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetProductsService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  apiCall(){
+    return this.http.get<Product[]>('http://localhost:8080/application/product');
+  }
+}
